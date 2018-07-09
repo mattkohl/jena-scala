858e0d51f840c96fdcdd1b18bf8b658deff4c7ed
mattkohl <hlaford@gmail.com>
2018-07-09 14:41:19 +0200

    Model convenience function

--- src/main/scala/jena/scala/ScalaModel.scala
+++ src/main/scala/jena/scala/ScalaModel.scala
@@ -1,8 +1,18 @@
 package jena.scala
 
+import org.apache.jena.rdf.model.{ModelFactory => MF}
+
 case class ScalaModel(model: Model) {
   def size: Long = model.size
   def createResource(uri: String): Resource = model.createResource(uri)
   def createProperty(nameSpace: String, localName: String): Property = model.createProperty(nameSpace, localName)
   def createLiteral(v: String, language: String): Literal = model.createLiteral(v, language)
+}
+
+object ScalaModel {
+  def apply[T <: Iterable[Statement]](statements: T): ScalaModel = {
+    val model: Model = MF.createDefaultModel
+    statements foreach model.add
+    new ScalaModel(model)
+  }
 }
--- src/test/scala/jena/scala/ModelSpec.scala
+++ src/test/scala/jena/scala/ModelSpec.scala
@@ -1,6 +1,6 @@
 package jena.scala
 
-import org.apache.jena.rdf.model.{ModelFactory => MF}
+import org.apache.jena.rdf.model.{ResourceFactory => RF, ModelFactory => MF}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
@@ -19,9 +19,23 @@ class ModelSpec extends WordSpec with Matchers {
 
       (model should have) size 1
     }
+    "build from an iterable of statements" in new Fixture {
+      val m = ScalaModel(Set(s1, s2))
+      (m should have) size 2
+    }
   }
 
   trait Fixture {
     val model: ScalaModel = MF.createDefaultModel.asScala
+    val s1: Statement = RF.createStatement(
+      RF.createResource("http://example.org/s1"),
+      RF.createProperty("http://example.org/p1"),
+      RF.createResource("http://example.org/o1")
+    )
+    val s2: Statement = RF.createStatement(
+      RF.createResource("http://example.org/s2"),
+      RF.createProperty("http://example.org/p2"),
+      RF.createResource("http://example.org/o2")
+    )
   }
 }
